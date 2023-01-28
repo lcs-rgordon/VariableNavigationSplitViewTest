3fa21d15cf37987c133f6bb3c6acc8f75f7b0b36
Russell Gordon <rgordon@lcs.on.ca>
2023-01-28 16:18:30 -0500

    Example of having sometimes a 2 column and sometimes a 3 column layout.

--- VariableNavigationSplitViewTest/ContentView.swift
+++ VariableNavigationSplitViewTest/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    @State private var selectedInt: Int? = 1
+    @State private var columnVisibility: NavigationSplitViewVisibility = .all
+
+    var sidebarList: some View {
+        List(1...10, id: \.self, selection: $selectedInt) { int in
+            NavigationLink("Row \(int)", value: int)
         }
-        .padding()
+        .navigationTitle("Sidebar")
+    }
+
+    var contentView: some View {
+        Text("Content \(selectedInt ?? 0)")
+    }
+
+    var detailView: some View {
+        Text("Detail \(selectedInt ?? 0)")
+    }
+
+    var body: some View {
+        Group {
+            // Only rows that are a multiple of 2 will have a two-column layout
+            if selectedInt?.isMultiple(of: 2) == true {
+                NavigationSplitView(columnVisibility: $columnVisibility) {
+                    sidebarList
+                } detail: {
+                    detailView
+                }
+            } else {
+                NavigationSplitView(columnVisibility: $columnVisibility) {
+                    sidebarList
+                } content: {
+                    contentView
+                } detail: {
+                    detailView
+                }
+            }
+        }
+        .navigationSplitViewStyle(.balanced)
     }
 }
 
