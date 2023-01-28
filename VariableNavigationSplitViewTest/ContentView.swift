91e8dd71e0ed97d0b52eb97bec3cecddc299c89e
Russell Gordon <rgordon@lcs.on.ca>
2023-01-28 16:20:15 -0500

    Using a list to indicate what layouts show with two columns.

--- VariableNavigationSplitViewTest/ContentView.swift
+++ VariableNavigationSplitViewTest/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var selectedInt: Int? = 1
     @State private var columnVisibility: NavigationSplitViewVisibility = .all
+    let doubleColumnOptions = [2, 3, 4]
 
     var sidebarList: some View {
         List(1...10, id: \.self, selection: $selectedInt) { int in
@@ -29,7 +30,7 @@ struct ContentView: View {
     var body: some View {
         Group {
             // Only rows that are a multiple of 2 will have a two-column layout
-            if selectedInt?.isMultiple(of: 2) == true {
+            if doubleColumnOptions.contains(selectedInt ?? 0) == true {
                 NavigationSplitView(columnVisibility: $columnVisibility) {
                     sidebarList
                 } detail: {
