293b25a465058763f929a6d61e848e034a363894
Russell Gordon <rgordon@lcs.on.ca>
2023-01-28 16:14:37 -0500

    Initial Commit

---
+++ VariableNavigationSplitViewTest/VariableNavigationSplitViewTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariableNavigationSplitViewTestApp.swift
+//  VariableNavigationSplitViewTest
+//
+//  Created by Russell Gordon on 2023-01-28.
+//
+
+import SwiftUI
+
+@main
+struct VariableNavigationSplitViewTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
